24e481f982b498b47be11398770e300e4883620e
InnaKeba <kebainna@gmail.com>
2025-06-13 22:58:22 +0300

    Added code for task 1

---
+++ hw-module_03.py
@@ -0,0 +1,13 @@
+#Завдання 1
+#Створіть функцію get_days_from_today(date), яка розраховує кількість днів між заданою датою і поточною датою.
+from datetime import datetime
+def get_days_from_today(date):
+    try:
+        parsed_date = datetime.strptime(date, "%Y-%m-%d").date()
+        today = datetime.today().date()
+        difference = (parsed_date - today).days
+        return difference
+    except ValueError:
+        return"Помилка: Неправильний формат дати. Використовуйте формат'РРРР-ММ-ДД'."
+print(get_days_from_today("2021-10-09"))
+print(get_days_from_today("21-10-09")) #Перевірка except
